9a4fd79517497fbf5bea941baac07946411da71e
Silvia Velicu <sveli@softserveinc.com>
2019-01-28 18:03:55 +0200

    Update styles and components.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { QuotesService } from '../../services/quotes.service';
+import { Quote } from '../../models/QuoteModel';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  featuredQuotes: Quote[];
+  constructor(private quoteService: QuotesService) {}
 
   ngOnInit() {
+    this.quoteService.getQuotes().subscribe(quotes => {
+      this.featuredQuotes = quotes.filter(quote => {
+        return quote.isFeatured === true;
+      });
+    });
   }
-
 }
