70e8ab4ffc5b501c4513c2c7e30f4c1d9bd1e5ad
Silvia Velicu <sveli@softserveinc.com>
2019-02-04 11:27:14 +0200

    Add authors page content.

--- src/app/components/authors/authors.component.ts
+++ src/app/components/authors/authors.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { QuotesService } from '../../services/quotes.service';
+import { Author } from '../../models/AuthorModel';
+
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
 export class AuthorsComponent implements OnInit {
+  authors: Author[];
 
-  constructor() { }
+  constructor(private quoteService: QuotesService) {}
 
   ngOnInit() {
+    this.quoteService.getAuthors().subscribe(authors => {
+      this.authors = authors;
+    });
   }
-
 }
