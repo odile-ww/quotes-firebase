680e1e2c469ac2dbf59bd94d3858d3f13f5cb9dc
Silvia Velicu <odileww@gmail.com>
2020-03-09 14:23:44 +0200

    Minor cleanup

--- src/app/components/quote-create/quote-create.component.ts
+++ src/app/components/quote-create/quote-create.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, FormArray, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
 
-import { QuotesService } from '../../services/quotes.service';
-import { Quote } from '../../models/QuoteModel';
+import { QuotesService } from "../../services/quotes.service";
+import { Quote } from "../../models/QuoteModel";
 
 @Component({
-  selector: 'app-quote-create',
-  templateUrl: './quote-create.component.html',
-  styleUrls: ['./quote-create.component.scss']
+  selector: "app-quote-create",
+  templateUrl: "./quote-create.component.html",
+  styleUrls: ["./quote-create.component.scss"]
 })
 export class QuoteCreateComponent implements OnInit {
   quoteForm: FormGroup;
-  mode = 'create';
+  mode = "create";
   quote: Quote;
   private quoteId: string;
 
@@ -24,19 +24,19 @@ export class QuoteCreateComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.quoteId = this.route.snapshot.params['id'];
+    this.quoteId = this.route.snapshot.params["id"];
 
     this.quoteForm = this.fb.group({
-      content: ['', [Validators.required, Validators.minLength(5)]],
+      content: ["", [Validators.required, Validators.minLength(5)]],
       isFeatured: false,
-      author: '',
-      title: '',
-      tags: this.fb.array([this.fb.control('')])
+      author: "",
+      title: "",
+      tags: this.fb.array([this.fb.control("")])
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has('quoteId')) {
-        this.mode = 'edit';
-        this.quoteId = paramMap.get('quoteId');
+      if (paramMap.has("quoteId")) {
+        this.mode = "edit";
+        this.quoteId = paramMap.get("quoteId");
         this.quotesService.getSingleQuote(this.quoteId).subscribe(quoteData => {
           this.quote = quoteData;
           this.quoteForm.patchValue({
@@ -46,27 +46,27 @@ export class QuoteCreateComponent implements OnInit {
             isFeatured: this.quote.isFeatured
           });
           this.quoteForm.setControl(
-            'tags',
+            "tags",
             this.fb.array(this.quote.tags || [])
           );
         });
       } else {
-        this.mode = 'create';
+        this.mode = "create";
         this.quoteId = null;
       }
     });
   }
 
   get content() {
-    return this.quoteForm.get('content');
+    return this.quoteForm.get("content");
   }
   get tags() {
-    return this.quoteForm.get('tags') as FormArray;
+    return this.quoteForm.get("tags") as FormArray;
   }
 
   addTag(e) {
     e.preventDefault();
-    this.tags.push(this.fb.control(''));
+    this.tags.push(this.fb.control(""));
   }
 
   deleteTag(i) {
@@ -77,19 +77,19 @@ export class QuoteCreateComponent implements OnInit {
     if (this.quoteForm.invalid) {
       return;
     }
-    if (this.mode === 'create') {
+    if (this.mode === "create") {
       this.quotesService.addQuote(value);
     } else {
       value.id = this.quoteId;
       this.quotesService.updateQuote(value);
     }
-    this.router.navigate(['/quotes']);
+    this.router.navigate(["/quotes"]);
   }
 
   deleteQuote(e) {
-    if (confirm('Are you sure you want to delete this quote?')) {
+    if (confirm("Are you sure you want to delete this quote?")) {
       this.quotesService.deleteQuote(this.quote);
     }
-    this.router.navigate(['/quotes']);
+    this.router.navigate(["/quotes"]);
   }
 }
