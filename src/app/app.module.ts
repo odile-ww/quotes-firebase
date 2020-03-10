4a6e5c42d53720cf90fb18f56632ae08b163e230
Silvia Velicu <sveli@softserveinc.com>
2020-03-10 14:05:53 +0200

    Add carousel to first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UrlSerializer } from '@angular/router';
-
-import { environment } from '../environments/environment';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { QuotesListComponent } from './components/quotes-list/quotes-list.component';
 import { AuthorsComponent } from './components/authors/authors.component';
@@ -17,7 +20,6 @@ import { QuoteCreateComponent } from './components/quote-create/quote-create.com
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 import { CustomSerializer } from './utils/custom-url-serializer';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -34,11 +36,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule.enablePersistence(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CarouselModule.forRoot()
   ],
-  providers: [
-    { provide: UrlSerializer, useClass: CustomSerializer }
-  ],
+  providers: [{ provide: UrlSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
