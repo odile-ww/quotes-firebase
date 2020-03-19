727882c407ad25fc0d9e42b8fa972140658f5890
Silvia Velicu <odileww@gmail.com>
2020-03-19 18:06:17 +0200

    Refine FE access rules

--- src/app/components/quotes-list/quotes-list.component.ts
+++ src/app/components/quotes-list/quotes-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { QuotesService } from '../../services/quotes.service';
 import { Quote } from '../../models/QuoteModel';
-
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-quotes-list',
@@ -14,19 +14,29 @@ export class QuotesListComponent implements OnInit {
   quotes: Quote[];
   hasFilter: boolean;
   filteredValue: string;
+  isLoggedIn: boolean;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private quoteService: QuotesService
-    ) { }
+    private quoteService: QuotesService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLoggedIn = true;
+      }
+    });
+
     const snapshotParams = Object.keys(this.route.snapshot.params),
       queryKey = snapshotParams[0];
 
     if (snapshotParams.length > 0) {
-      const serializedParamValue = this.serializeUrl(this.route.snapshot.params.author);
+      const serializedParamValue = this.serializeUrl(
+        this.route.snapshot.params.author
+      );
 
       this.filterQuotesByKey(queryKey, serializedParamValue);
     } else {
