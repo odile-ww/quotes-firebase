6fc58f2eba3093ade9e9bf32288d1a5db798f742
Silvia Velicu <odileww@gmail.com>
2021-01-07 17:24:47 +0200

    Fix some packages and object props

--- src/app/services/quotes.service.ts
+++ src/app/services/quotes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection,
-  AngularFirestoreDocument
+  AngularFirestoreDocument,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,7 +11,7 @@ import { Quote } from '../models/QuoteModel';
 import { Author } from '../models/AuthorModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuotesService {
   quotesCollection: AngularFirestoreCollection<Quote>;
@@ -24,39 +24,37 @@ export class QuotesService {
   message: string;
 
   constructor(private afs: AngularFirestore) {
-    this.quotesCollection = this.afs.collection('quotes', ref =>
+    this.quotesCollection = this.afs.collection('quotes', (ref) =>
       ref.orderBy('author')
     );
-    this.authorsCollection = this.afs.collection('autors', ref =>
+    this.authorsCollection = this.afs.collection('autors', (ref) =>
       ref.orderBy('lastName')
     );
   }
 
   getQuotes(): Observable<Quote[]> {
     this.message = undefined;
-    return this.quotesCollection
-      .snapshotChanges()
-      .pipe(
-        map(changes => {
-          return changes.map(action => {
-            return <Quote>{
-              id: action.payload.doc.id,
-              ...action.payload.doc.data()
-            };
-          });
-        })
-      );
+    return this.quotesCollection.snapshotChanges().pipe(
+      map((changes) => {
+        return changes.map((action) => {
+          return <Quote>{
+            id: action.payload.doc['id'],
+            ...action.payload.doc.data(),
+          };
+        });
+      })
+    );
   }
 
   getQuotesByKey(key, value): Observable<Quote[]> {
     return this.afs
-      .collection('quotes', ref => ref.where(key, '==', value))
+      .collection('quotes', (ref) => ref.where(key, '==', value))
       .snapshotChanges()
       .pipe(
-        map(snaps => {
-          return snaps.map(snap => {
+        map((snaps) => {
+          return snaps.map((snap) => {
             const data = snap.payload.doc.data() as Quote;
-            data.id = snap.payload.doc.id;
+            data.id = snap.payload.doc['id'];
             return data;
           });
         })
@@ -65,13 +63,13 @@ export class QuotesService {
 
   getQuotesByTag(tag): Observable<Quote[]> {
     return this.afs
-      .collection('quotes', ref => ref.where('tags', 'array-contains', tag))
+      .collection('quotes', (ref) => ref.where('tags', 'array-contains', tag))
       .snapshotChanges()
       .pipe(
-        map(snaps => {
-          return snaps.map(snap => {
+        map((snaps) => {
+          return snaps.map((snap) => {
             const data = snap.payload.doc.data() as Quote;
-            data.id = snap.payload.doc.id;
+            data.id = snap.payload.doc['id'];
             return data;
           });
         })
@@ -80,20 +78,18 @@ export class QuotesService {
 
   getSingleQuote(id: string): Observable<Quote> {
     this.quoteDoc = this.afs.doc<Quote>(`quotes/${id}`);
-    return this.quoteDoc
-      .snapshotChanges()
-      .pipe(
-        map(action => {
-          if (action.payload.exists === false) {
-            return null;
-          } else {
-            return <Quote>{
-              id: action.payload.id,
-              ...action.payload.data()
-            };
-          }
-        })
-      );
+    return this.quoteDoc.snapshotChanges().pipe(
+      map((action) => {
+        if (action.payload.exists === false) {
+          return null;
+        } else {
+          return <Quote>{
+            id: action.payload['id'],
+            ...action.payload.data(),
+          };
+        }
+      })
+    );
   }
 
   addQuote(quote: Quote) {
@@ -112,17 +108,15 @@ export class QuotesService {
   }
 
   getAuthors(): Observable<Author[]> {
-    return this.authorsCollection
-      .snapshotChanges()
-      .pipe(
-        map(changes => {
-          return changes.map(action => {
-            return {
-              id: action.payload.doc.id,
-              ...action.payload.doc.data()
-            };
-          });
-        })
-      );
+    return this.authorsCollection.snapshotChanges().pipe(
+      map((changes) => {
+        return changes.map((action) => {
+          return {
+            id: action.payload.doc['id'],
+            ...action.payload.doc.data(),
+          };
+        });
+      })
+    );
   }
 }
